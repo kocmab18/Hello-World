7d5ddc15e006c35ad3e7bf37f74e3de1dc8364b2
David Tantscher <tandab18@htl-kaindorf.at>
2022-05-25 16:00:32 +0200

    new line

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello from Me Too David THE GREAT");
     }
+
 }
